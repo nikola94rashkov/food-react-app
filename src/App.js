669f8ba5fee0caf90bf0b22ad490909d83570cd6
Nikola Rashkov <nikola94rashkov@gmail.com>
2021-12-04 17:28:30 +0200

    Add react router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+     {/* <Routes>
+        <Route path="/" element={<HomeView />}></Route>
+        <Route path="/blogs/details" element={<BlogDetailsView />} />
+        <Route path="/login" element={<LoginView />}/>
+        <Route path="/registration" element={<RegistrationView />}/>
+        <Route path="*" element={<ErrorView />}/>
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
