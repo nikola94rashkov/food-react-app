60e0512a6fd9f46524475804f426076b9014ad47
Nikola Rashkov <nikola94rashkov@gmail.com>
2021-12-13 00:27:39 +0200

    Add latest changed

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,7 +1,24 @@
-import './Grid.scss';
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../services/firebase';
 import GridItem from '../GridItem/GridItem';
+import './Grid.scss';
 
 const Grid = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() =>{
+        fetchArticles();
+    }, [])
+
+    const fetchArticles = async() =>{
+        const querySnapshot = await getDocs(collection(db, "articles"));
+
+        querySnapshot.forEach((doc) => {
+            setArticles([...articles, doc.data()])
+        });
+    }
+
     return (
         <section className="section-grid">
             <div className="shell">
@@ -13,7 +30,7 @@ const Grid = () => {
                     <div className="grid">
                         <div className="grid__row">
                             <div className="grid__col grid__col--1of4">
-                                <GridItem/>
+                                { articles.map(x => <GridItem data={x}/>) }
                             </div>
                         </div>
                     </div>
--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -2,18 +2,18 @@ import { Link } from 'react-router-dom';
 import './GridItem.scss';
 
 const GridItem = (
-    recipe
+    data
 ) => {
     return (
         <article className="article-recipe">
-            <Link to={`/recipe/{id}`}></Link>
+            <Link to={`/recipe/${data.id}`}></Link>
 
             <div className="article__image image-fit">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFvhvAVB5VgZ0W6xltgTTMva4_gi-T8y1VBg&usqp=CAU" alt="recipe" />
+                <img src={data.imageUrl} alt="recipe" />
             </div>
 
             <div className="article__content">
-                <h5>Свинско със зеле</h5>
+                <h5>{data.title}</h5>
             </div>
         </article> 
     );
